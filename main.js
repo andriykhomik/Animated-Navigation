99484f596c1ac8f78c36cc792056f7ba6eaa0d88
andriykhomik <andriykhomik1992@gmail.com>
2021-11-13 19:44:39 +0200

    finished this project by my implementation of JS file

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
 const toggle = document.querySelector('#toggle');
 const nav = document.querySelector('#nav');
 
-const idToggle = document.getElementById('toggle');
-
 toggle.addEventListener('click', ()=> nav.classList.toggle('active'));
